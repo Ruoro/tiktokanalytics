113a6f5e26bc39c94b9edac40dbeb8b4cf432c53
ruoroj <you@example.com>
2022-03-02 05:12:11 -0500

    tiktok failed

---
+++ tiktok.py
@@ -0,0 +1,9 @@
+# import TikTokApi sdk
+from TikTokApi import TikTokApi 
+# the json is for the export of data 
+import json
+
+api = TikTokApi()
+
+for video in api.trending.videos():
+    print(video.author.username)
